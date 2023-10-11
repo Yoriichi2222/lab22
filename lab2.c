cf2a8de644eba1e07c26680d70316f05bb82fade
Yoriichi2222 <vs973222@gmail.com>
2023-10-11 22:39:45 +0500

    111
    11

--- lab2.c
+++ lab2.c
@@ -4,18 +4,19 @@
 int main()
 {
     double x = 0.0, y;
-    double eps;
+    double mat;
     double h;
     scanf("%lf", &h);
-    eps = h / 2.0;
+    mat = h / 2.0;
     printf("x\t\ty\n");
-do {
-    if(x <= 1.0 + eps)
+do 
+{
+    if(x <= 1.0 + mat)
           y = 8 * pow(x, 3) * cos(x);
         else
           y = log(1 + sqrt(x)) - cos(x);
         printf("%f\t%f\n", x, y);
         x = x + h;
-} while(x >= 0.0 && x <= 2.0 + eps);
+} while(x >= 0.0 && x <= 2.0 + mat);
   return 0;
 }
